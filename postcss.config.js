386ade87a483fd98e1943fbfc14374d5b48fd8dc
22mahmoud <mahmoudss1197@gmail.com>
2020-02-19 03:10:28 +0200

    integrate w/ tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+	content: ["./src/**/*.svelte", "./src/**/*.html"],
+	defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+module.exports = {
+	plugins: [
+		require("tailwindcss"),
+		...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+	]
+};
