530c281565d54eeb6af9d4529b9e8737385f4b13
22mahmoud <mahmoudss1197@gmail.com>
2020-02-20 02:57:42 +0200

    home page typewriter with display font

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,7 +6,7 @@
 
 <Cursor />
 <main class="bg-gray-900 text-gray-300 w-screen h-screen ">
-  <div class="max-w-4xl mx-auto">
+  <div class="max-w-4xl h-full mx-auto">
     <slot />
   </div>
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,16 @@
+<script>
+  import Typewriter from '../components/Typewriter.svelte';
+</script>
+
 <svelte:head>
   <title>Sapper project template</title>
 </svelte:head>
 
-
-<h1>Hey</h1>
+<div class="h-full flex items-center content-center ">
+  <Typewriter>
+    Hey, I'm Mahmoud Ashraf. A Software Engineer and Love Javascript, (maybe).
+  </Typewriter>
+</div>
 
 <style>
 
