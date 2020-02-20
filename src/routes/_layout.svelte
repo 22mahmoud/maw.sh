0200a36b6094a31f900b0669f6902d8187ef786f
22mahmoud <mahmoudss1197@gmail.com>
2020-02-20 07:55:38 +0200

    remove cursor and typewriter

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,12 +5,10 @@
   import Loader from '../components/Loader.svelte';
 
   import Nav from '../components/Nav.svelte';
-  import Cursor from '../components/Cursor/index.svelte';
   export let segment;
   const { preloading } = stores();
 </script>
 
-<Cursor />
 <PageLoadingBar color1="#81e6d9" color2="#4FD1C5" {preloading} />
 <main transition:fade class="bg-gray-900 text-gray-300 w-screen h-screen ">
   <div class="max-w-4xl h-full mx-auto flex flex-col">
