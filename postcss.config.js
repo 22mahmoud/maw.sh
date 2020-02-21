091722faf2c491134db99c49873f9176f79ff71c
22mahmoud <mahmoudss1197@gmail.com>
2020-02-21 21:27:35 +0200

    add some plugins to postcss

--- postcss.config.js
+++ postcss.config.js
@@ -7,5 +7,15 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 const production = process.env.NODE_ENV !== 'development';
 
 module.exports = {
-  plugins: [require('tailwindcss'), ...(production ? [purgecss] : [])],
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-fail-on-warn'),
+    production && purgecss,
+    production &&
+      require('cssnano')({
+        preset: 'default',
+      }),
+  ].filter(Boolean),
 };
