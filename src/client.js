08eb67e98e45446bda2c16ec5b88f4bff58a3249
22mahmoud <mahmoudss1197@gmail.com>
2020-02-21 19:46:38 +0200

    fix require error

--- src/client.js
+++ src/client.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 import * as sapper from '@sapper/app';
 
 sapper.start({
--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 import polka from 'polka';
 import sirv from 'sirv';
 import compression from 'compression';
