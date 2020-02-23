181ab44e6ddca7dd77efbf6608a8a47119a0f5e0
22mahmoud <mahmoudss1197@gmail.com>
2020-02-23 18:21:13 +0200

    add thumb default image

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -4,7 +4,7 @@
   export let description = siteMetadata.description;
   export let keywords = siteMetadata.keywords;
   export let slug = null;
-  export let thumb = null;
+  export let thumb = siteMetadata.image;
 </script>
 
 <svelte:head>
