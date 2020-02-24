b5ec21c0e947ebc5d88af3564043e03183a9d0ab
22mahmoud <mahmoudss1197@gmail.com>
2020-02-24 08:52:13 +0200

    add cool svg from styled-system on top page

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -7,7 +7,44 @@
   ];
 </script>
 
-<nav>
+<div class="flex absolute top-0 left-0 w-full ">
+  <svg width="100%" height="40" style="overflow:visible">
+    <defs>
+      <pattern
+        id="hex"
+        patternUnits="userSpaceOnUse"
+        patternTransform="translate(-3.5 0)"
+        viewBox="-14 -16 28 48"
+        width="26"
+        height="66"
+      >
+        <g fill="none" stroke-width="1" stroke="currentcolor">
+          <path
+            d=" M0 -16 L-14 -8 L -14 8 L 0 16 L 14 8 L 14 -8 L 0 -16 z "
+          ></path>
+          <path
+            d=" M0 -16 L-14 -8 L -14 8 L 0 16 L 14 8 L 14 -8 L 0 -16 z "
+            transform="translate(14 24)"
+          ></path>
+          <g stroke-width="1" opacity="0.5">
+            <path d=" M0 -16 L 0 0 M-14 8 L0 0 L14 8 "></path>
+            <path
+              d=" M0 -16 L 0 0 M-14 8 L0 0 L14 8 "
+              transform="translate(14 24)"
+            ></path>
+            <path
+              d=" M0 -16 L 0 0 M-14 8 L0 0 L14 8 "
+              transform="translate(-14 24)"
+            ></path>
+          </g>
+        </g>
+      </pattern>
+    </defs>
+    <rect width="100%" height="100%" fill="url(#hex)"></rect>
+  </svg>
+</div>
+
+<nav class="my-12">
   <ul class="flex py-8">
     {#each routes as { key, value }}
       <li class="mx-6">
