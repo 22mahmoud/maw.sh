24f3d2912f6b10b2e175f28098e5d63f68c7bc5a
22mahmoud <mahmoudss1997@gmail.com>
2020-08-11 18:38:42 +0200

    :wrench: add some usfel middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
 
 const port = process.env.PORT || 8000;
+
+app.use(compression());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use(helmet());
 
 app.get('/', (_req, res) => {
   res.send('Hello, World');
