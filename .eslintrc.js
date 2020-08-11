8affeb9ddab046ba00850cfc9d6ba5c795bf6763
22mahmoud <mahmoudss1997@gmail.com>
2020-08-11 18:12:52 +0200

    :sparkles: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.tooling.json'],
+  },
+  extends: [
+    'airbnb-typescript/base',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    '@typescript-eslint/no-empty-interface': 'off',
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.get("/", (_req, res) => {
-  res.send("Hello, World");
+app.get('/', (_req, res) => {
+  res.send('Hello, World');
 });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App is running at http://localhost:${port}`);
-  console.log("  Press CTRL-C to stop\n");
 });
