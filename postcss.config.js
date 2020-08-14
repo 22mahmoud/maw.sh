84e45ecc43289dcda34f0bd0ec7a1498f49357cb
22mahmoud <mahmoudss1997@gmail.com>
2020-08-14 08:31:59 +0200

    🎨 use normlizecss and purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,30 @@
 const postcssImport = require('postcss-import');
+const purgecss = require('@fullhuman/postcss-purgecss');
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
 const { paths } = require('./config/paths');
+
+const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
   plugins: [
     postcssImport({
-      path: [paths.src, `${__dirname}/node_modules`],
+      path: [paths.src, `node_modules`],
     }),
 
     autoprefixer(),
-  ],
+
+    production &&
+      purgecss({
+        content: [`${paths.src}/**/*.{ts,pug}`],
+      }),
+
+    production &&
+      cssnano({
+        preset: ['default', { discardComments: { removeAll: true } }],
+      }),
+  ].filter(Boolean),
 
   sourceMap: true,
 };
