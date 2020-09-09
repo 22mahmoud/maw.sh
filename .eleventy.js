7fdb3d4fccb9318e8669fc120ff55f356907ba40
22mahmoud <mahmoudss1997@gmail.com>
2020-09-09 19:40:16 +0200

    add body font and get posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,23 @@
 module.exports = (cfg) => {
   cfg.addPassthroughCopy("src/assets/fonts");
 
+  cfg.addPassthroughCopy("src/**/*.jpg");
+  cfg.addPassthroughCopy("src/**/*.jpeg");
+  cfg.addPassthroughCopy("src/**/*.gif");
+
+  cfg.addCollection("latestPosts", function(collectionApi) {
+    return collectionApi
+      .getFilteredByTag("posts")
+      .reverse()
+      .slice(0, 4);
+  });
+
   return {
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+
     dir: {
       input: "src",
       output: "site",
