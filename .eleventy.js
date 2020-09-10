85b1b86479b50d77c9bdf3dcf35399b9400e9b5d
22mahmoud <mahmoudss1997@gmail.com>
2020-09-10 04:44:11 +0200

    simple homepage

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,29 @@
+const { format, parseISO } = require('date-fns');
+
 module.exports = (cfg) => {
-  cfg.addPassthroughCopy("src/assets/fonts");
+  cfg.addPassthroughCopy('src/assets/fonts');
 
-  cfg.addPassthroughCopy("src/**/*.jpg");
-  cfg.addPassthroughCopy("src/**/*.jpeg");
-  cfg.addPassthroughCopy("src/**/*.gif");
+  cfg.addPassthroughCopy('src/**/*.jpg');
+  cfg.addPassthroughCopy('src/**/*.jpeg');
+  cfg.addPassthroughCopy('src/**/*.gif');
 
-  cfg.addCollection("latestPosts", function(collectionApi) {
-    return collectionApi
-      .getFilteredByTag("posts")
-      .reverse()
-      .slice(0, 4);
+  cfg.addCollection('latestPosts', function (collectionApi) {
+    return collectionApi.getFilteredByTag('posts').reverse().slice(0, 3);
   });
 
+  cfg.addFilter('date', (date) => format(parseISO(date), 'd MMMM, yyyy'));
+
   return {
-    templateFormats: ["md", "njk", "html"],
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
 
     dir: {
-      input: "src",
-      output: "site",
-      includes: "_includes",
-      layouts: "_layouts",
+      input: 'src',
+      output: 'site',
+      includes: '_includes',
+      layouts: '_layouts',
     },
   };
 };
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 const postcssPresetEnv = require('postcss-preset-env');
 const cssnano = require('cssnano');
+const postcssFunctions = require('postcss-functions');
+const color = require('css-color-converter');
+
 const postcssPurgecss = require('@fullhuman/postcss-purgecss');
 const rfs = require('rfs/postcss');
 
@@ -10,6 +13,7 @@ const plugins = [
     stage: 3,
     features: {
       'nesting-rules': true,
+      'not-pseudo-class': true,
     },
   }),
 
@@ -26,6 +30,19 @@ const plugins = [
     remValue: 16,
   }),
 
+  postcssFunctions({
+    functions: {
+      darken: (value, frac) => {
+        console.log(value);
+        const darken = 1 - parseFloat(frac);
+        const [r, g, b] = color(value)
+          .toRgbaArray()
+          .map((val) => val * darken);
+
+        return color([r, g, b]).toHexString();
+      },
+    },
+  }),
   isProd &&
     postcssPurgecss({
       content: ['./site/**/*.html'],
