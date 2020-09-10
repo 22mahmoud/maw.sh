da4d6d7eff3560a656a343ce87f01fdf142dba66
22mahmoud <mahmoudss1997@gmail.com>
2020-09-11 00:51:30 +0200

    add workbox sw

--- src/js/app.js
+++ src/js/app.js
@@ -26,3 +26,20 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('turbolinks:load', () => {
   lazyload.update();
 });
+
+const isProd = process.env.NODE_ENV === 'production';
+
+if (isProd && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      /* eslint-disable */
+      .then(() => {
+        console.log('sw.js is loaded');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    /* eslint-enable */
+  });
+}
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  globDirectory: 'build/',
+  globPatterns: [
+    '**/*.{js,ico,woff,woff2,jpeg,png,json,html,gif,webp,txt,xml,css}',
+  ],
+  swDest: 'build/sw.js',
+};
