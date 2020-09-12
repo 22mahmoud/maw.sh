fb1dd50cd5b7f9c67130df714fbc698465e6f541
Mahmoud Ashraf <mahmoudss1997@gmail.com>
2020-09-12 14:32:05 +0200

    Dev (#25)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,31 +1,33 @@
 import Lazyload from 'vanilla-lazyload';
 import turbolinks from 'turbolinks';
 
-const lazyload = new Lazyload();
-
-turbolinks.start();
-
-document.addEventListener('DOMContentLoaded', () => {
-  lazyload.update();
-});
-
-document.addEventListener('turbolinks:load', () => {
-  lazyload.update();
-});
-
 const isProd = process.env.NODE_ENV === 'production';
 
-if (isProd && 'serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      /* eslint-disable */
-      .then(() => {
-        console.log('sw.js is loaded');
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    /* eslint-enable */
+if (isProd) {
+  const lazyload = new Lazyload();
+
+  turbolinks.start();
+
+  document.addEventListener('DOMContentLoaded', () => {
+    lazyload.update();
   });
+
+  document.addEventListener('turbolinks:load', () => {
+    lazyload.update();
+  });
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        /* eslint-disable */
+        .then(() => {
+          console.log('sw.js is loaded');
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      /* eslint-enable */
+    });
+  }
 }
