af02c92ee43fe1545aea730694d23df81c3a76f8
22mahmoud <hello@mahmoudashraf.dev>
2024-07-10 14:14:26 +0300

    enhance

--- assets/js/webmentions.js
+++ assets/js/webmentions.js
@@ -1,13 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function getRandomColor() {
-    const hue = Math.floor(Math.random() * 360);
-    const saturation = Math.floor(Math.random() * 50) + 50;
-    const lightness = 80;
+function getRandomColor() {
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = Math.floor(Math.random() * 20) + 60;
+  const lightness = Math.floor(Math.random() * 15) + 75;
 
-    return `hsla(${hue}, ${saturation}%, ${lightness}%, 0.4)`;
-  }
+  return `hsla(${hue}, ${saturation}%, ${lightness}%, 0.6)`;
+}
 
-  document
-    .querySelectorAll(".bookmark-item img")
-    .forEach((x) => x.style.setProperty("--hover-color", getRandomColor()));
-});
+document
+  .querySelectorAll(".bookmark-item img")
+  .forEach((x) => x.style.setProperty("--hover-color", getRandomColor()));
