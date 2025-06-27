f53b0d1aa9d48751f6bc5c27fbf3ac0e787b0d4a
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2025-06-28 00:53:14 +0300

    add fonts

---
+++ tailwind.config.ts
@@ -0,0 +1,50 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  theme: {
+    extend: {
+      typography: () => ({
+        main: {
+          css: {
+            '--tw-prose-body': 'var(--color-primary)',
+            '--tw-prose-headings': 'var(--color-primary)',
+            '--tw-prose-lead': 'var(--color-accent)',
+            '--tw-prose-links': 'var(--color-accent)',
+            '--tw-prose-bold': 'var(--color-primary)',
+            '--tw-prose-counters': 'var(--color-neutral-700)',
+            '--tw-prose-bullets': 'var(--color-accent)',
+            '--tw-prose-hr': 'var(--color-neutral-800)',
+            '--tw-prose-quotes': 'var(--color-primary)',
+            '--tw-prose-quote-borders': 'var(--color-neutral-800)',
+            '--tw-prose-captions': 'var(--color-primary)',
+            '--tw-prose-code': 'var(--color-accent)',
+            '--tw-prose-pre-code': 'var(--color-white)',
+            '--tw-prose-pre-bg': 'var(--color-darker)',
+            '--tw-prose-th-borders': 'var(--color-neutral-700)',
+            '--tw-prose-td-borders': 'var(--color-neutral-800)',
+
+            // Dark/inverted mode
+            '--tw-prose-invert-body': 'var(--color-primary)',
+            '--tw-prose-invert-headings': 'var(--color-white)',
+            '--tw-prose-invert-lead': 'var(--color-accent-bright)',
+            '--tw-prose-invert-links': 'var(--color-accent-bright)',
+            '--tw-prose-invert-bold': 'var(--color-white)',
+            '--tw-prose-invert-counters': 'var(--color-neutral-700)',
+            '--tw-prose-invert-bullets': 'var(--color-accent)',
+            '--tw-prose-invert-hr': 'var(--color-dark)',
+            '--tw-prose-invert-quotes': 'var(--color-primary)',
+            '--tw-prose-invert-quote-borders': 'var(--color-dark)',
+            '--tw-prose-invert-captions': 'var(--color-secondary)',
+            '--tw-prose-invert-code': 'var(--color-accent-bright)',
+            '--tw-prose-invert-pre-code': 'var(--color-white)',
+            '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
+            '--tw-prose-invert-th-borders': 'var(--color-neutral-700)',
+            '--tw-prose-invert-td-borders': 'var(--color-neutral-800)',
+          },
+        },
+      }),
+    },
+  },
+};
+
+export default config;
