0f881b460cca2f63c78dd0e648f112bc708472c2
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2025-07-04 20:25:38 +0300

    add postgres

--- config/django/base.py
+++ config/django/base.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "wagtail",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
