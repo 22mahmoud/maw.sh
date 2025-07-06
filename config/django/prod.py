8dd10a4d7eb42c82cf365a91f4cb24fda27db5b2
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2025-07-06 10:38:37 +0300

    add logging

--- config/django/prod.py
+++ config/django/prod.py
@@ -1,14 +1,31 @@
 from config.env import env
 
 from .base import *  # noqa: F403
-from .base import MIDDLEWARE, STORAGES
+from .base import MIDDLEWARE, STORAGES, BASE_DIR
 
 MIDDLEWARE.append("whitenoise.middleware.WhiteNoiseMiddleware")
 STORAGES["staticfiles"]["BACKEND"] = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
 )
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": env.str("DJANGO_LOG_LEVEL", "INFO"),
+        },
+    },
+}
