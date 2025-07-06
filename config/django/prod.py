b7cb610cec33b6ae7673866b390ffc4c78f5d756
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2025-07-06 12:25:08 +0300

    fix typo

--- config/django/prod.py
+++ config/django/prod.py
@@ -14,7 +14,7 @@ DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
-CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS ", default=[])
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 LOGGING = {
     "version": 1,
