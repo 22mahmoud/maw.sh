0765559d49692738acf8fbcccabc870bda94bf6e
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2025-08-17 14:31:47 +0300

    enhance comment honeypot

--- src/comments/forms.py
+++ src/comments/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django_comments_xtd.forms import XtdCommentForm as DjangoXtdCommentForm
 
 from src.base.renderers import FormRenderer
@@ -5,3 +6,31 @@ from src.base.renderers import FormRenderer
 
 class XtdCommentForm(DjangoXtdCommentForm):
     default_renderer = FormRenderer()
+    honeypot = forms.CharField(
+        required=False,
+        label="Phone number",
+        widget=forms.widgets.TelInput(  # type: ignore
+            attrs={
+                "autocomplete": "tel",
+                "placeholder": "Your phone number",
+                "tabindex": "-1",
+                "style": "position:absolute; left:-9999px;",
+                "aria-hidden": "true",
+            }
+        ),
+    )
+
+    def clean_phone(self):
+        value = self.cleaned_data["phone"]
+        if value:
+            raise forms.ValidationError("error")
+        return value
+
+    def security_errors(self):
+        errors = super().security_errors()
+
+        for f in ["phone"]:
+            if f in self.errors:
+                errors[f] = self.errors[f]
+
+        return errors
